8784d33e5efd08ac6c1085e704ef5e1f5dcf2e09
emanuelandriato <emanuelandriato@gmail.com>
2016-09-02 19:56:44 -0300

    Previsao do tempo
    realizados ajustes para trazer as previsoes pelo retrofit

--- app/src/main/java/br/com/posmobile/previsaodotempo/PrevisoesAPI.java
+++ app/src/main/java/br/com/posmobile/previsaodotempo/PrevisoesAPI.java
@@ -2,6 +2,7 @@ package br.com.posmobile.previsaodotempo;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -9,8 +10,7 @@ import retrofit2.http.Query;
  */
 
 public interface PrevisoesAPI {
-
+    //todo Inclua um parametro para passar dinamicamente o APPID como String
     @GET("daily?mode=json&lang=pt&units=metric&cnt=14")
-    Call<Previsoes> getPrevisoes(@Query("q") String cidade);//todo Inclua um parametro para passar dinamicamente o APPID como String
-
+    Call<Previsoes> getPrevisoes(@Query("q") String cidade_pais,@Query("APPID") String api_key);
 }
--- app/src/main/java/br/com/posmobile/previsaodotempo/Utils.java
+++ app/src/main/java/br/com/posmobile/previsaodotempo/Utils.java
@@ -7,7 +7,8 @@ package br.com.posmobile.previsaodotempo;
 
 public class Utils {
 
-    public static final String API_KEY = "de0c7357d361be8987b08ef3354969bb";
+    //public static final String API_KEY = "de0c7357d361be8987b08ef3354969bb"; chave do prof
+    public static final String API_KEY = "71a133bbb7f444fa7acf757df6e4b3ed";
     public static final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast/";
     public static final String URL_ICONE = "http://openweathermap.org/img/w/%s.png";
 
